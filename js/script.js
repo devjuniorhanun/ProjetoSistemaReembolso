8c5ceae802df4c332d30d120121e8294cc8354f5
Winston Hanun Júnior <devjuniorhanun@gmail.com>
2025-03-27 03:55:11 -0300

    Criando uma nova despesa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+// Selecionando os elementos do forumlário.
+// Pegando os dados do formulários
+const form = document.querySelector("form")
 // Pegando os valores de amount
 const amount = document.getElementById("amount")
+// Nome da despesa
+const expense = document.getElementById("expense")
+// Nome da Categoria da despeda
+const category = document.getElementById("category")
 
 // Manipulando o input amount para receber somente números
 amount.oninput = () => {
@@ -23,4 +30,22 @@ function formatCurrencyBRL(value) {
   return value
   //return value.replace("R$", "")
 
+}
+
+// Captura o evento de Submit do formulário para obter os valores
+form.onsubmit = (event) => {
+  // Desativando a submissão do formulário
+  event.preventDefault()
+
+  // Criando uma nova despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+  // Adiciona uma nova despesa 
+  expenseAdd(newExpense)
 }
